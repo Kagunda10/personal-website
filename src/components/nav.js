9f9689231c199a7f0bd44f5b182b3a9b05b6ddc6
Caleb Njiiri <caleb.njiiri@gmail.com>
2020-06-03 21:00:02 +0300

    updated link to blog

--- src/components/nav.js
+++ src/components/nav.js
@@ -294,7 +294,7 @@ class Nav extends Component {
                 <CSSTransition classNames={fadeDownClass} timeout={timeout}>
                   <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
                     <StyledResumeButton
-                      href="https://kagunda10.github.io"
+                      href="http://blog.calebnjiiri.dev"
                       target="_blank"
                       rel="nofollow noopener noreferrer">
                       Blog
